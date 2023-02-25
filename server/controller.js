7867b866a23433667e8284c254a0d21bd84a3804
Lily Springer <lilyspringer@hotmail.com>
2023-02-25 16:44:17 -0700

    exercise complete

--- server/controller.js
+++ server/controller.js
@@ -42,10 +42,34 @@ module.exports = {
     // },
 
     createHouse: (req, res) => {
-
+        let { address, price, imageURL } = req.body;
+        let newHouse = {
+            id: globalId,
+            address,
+            price,
+            imageURL
+        }
+        houses.push(newHouse);
+        res.status(200).send(houses);
+        globalId++
     },
 
     updateHouse: (req, res) => {
+        let { id } = req.params;
+        let { type } = req.body;
+        let index = houses.findIndex(elem => elem.id === +req.params.id)
 
-    }
+        if (houses[index].price <= 10,000 && type === 'minus') {
+            houses[index].price = 0
+            res.status(200).send(houses)
+        } else if (type === 'plus') {
+            houses[index].prices += 10,000
+            res.status(200).send(houses)
+        } else if (type === 'minus') {
+            houses[index].prices -= 10,000
+            res.status(200).send(houses)
+        } else {
+            res.sendStatus(400)
+        }
+    } 
 }
